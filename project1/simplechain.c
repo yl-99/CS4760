4d61dd73864d9f4f9101b92f3b27f2c714d675f6
YL99 <yl99@YL99s-MacBook-Pro.local>
2022-02-07 20:21:10 -0600

    getopt logic

--- project1/simplechain.c
+++ project1/simplechain.c
@@ -1,21 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+
    pid_t childpid = 0;
-   int i, n;
+   int i, numOfProcesses = 4, numOfChars = 80, sleepTime = 3, numOfIterations;
 
-   if (argc != 2) {
-      fprintf(stderr, "Usage: %s processes\n",argv[0]);
+   int option;
+   while ((option = getopt(argc, argv, "h:p:c:s:i")) != -1) // :pcsi
+   {
+      switch (option)
+      {
+      case 'h':
+         fprintf(stderr, "Usage: %s [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile \n", argv[0]);
+         return 1;
+         break;
+      case 'p':
+      fprintf(stderr, "proc %i \n", atoi(optarg));
+         if(optarg != NULL)
+         {
+            numOfProcesses = atoi(optarg);
+         }
+         else
+         {
+            numOfProcesses = atoi(argv[3]);
+         }
+         
+         
+         break;
+      case 'c':
+       fprintf(stderr, "chars %i \n", numOfChars);
+      if(optarg != NULL)
+         {
+            numOfChars = atoi(optarg);
+         }
+         else
+         {
+            numOfChars = atoi(argv[3]);
+         }
+         
+         fprintf(stderr, "chars %i \n", numOfChars);
+         break;
+      case 's':
+         sleepTime = atoi(argv[4]);
+         fprintf(stderr, "sleep %i \n", sleepTime);
+         break;
+      case 'i':
+         numOfIterations = atoi(argv[5]);
+         fprintf(stderr, "iterations %i \n", numOfIterations);
+         break;
+      default:
+         //never enters
+         break;
+      }
+   }
+
+   if (argc < 2)
+   {
+      perror("Chain: Error: Try ./simplechain -h for help \n");
       return 1;
    }
-   n = atoi(argv[1]);
-   for (i = 1; i < n; i++)
+
+   for (i = 1; i < numOfProcesses; i++)
       if (childpid = fork())
          break;
+
    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-      i, (long)getpid(), (long)getppid(), (long)childpid);
+           i, (long)getpid(), (long)getppid(), (long)childpid);
 
    return 0;
 }
