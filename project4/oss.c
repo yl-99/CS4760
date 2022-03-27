348fc21f4e7086640ef0ecb3bcd7144024cdd03d
YL99 <yl99@YL99s-MacBook-Pro.local>
2022-03-27 17:21:42 -0500

    oss makefile readme

---
+++ project4/oss.c
@@ -0,0 +1,23 @@
+
+// Program by: YOUSEF LANGI 4/7/22
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+    
+    
+}
---
+++ project4/readme.md
@@ -0,0 +1,8 @@
+Yousef Langi 3/17/22
+
+To compile and execute program:
+1. cd into prjoect4 folder
+2. Execute 'make' on command line to compile the oss program. Make file located within the file called "Makefile" 
+3. Execute './oss'
+
+Github - https://github.com/yl-99/CS4760/tree/project4
